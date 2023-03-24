707b829c7d8aa5c50c5888f9c31c632ad2d127df
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2023-03-24 00:41:45 -0400

    Add better logging for our custom jsonconverter for storages

--- Extensions/StorageConverter.cs
+++ Extensions/StorageConverter.cs
@@ -20,7 +20,12 @@ public class TypeDiscriminatorConverter : JsonConverter<IStorageConfiguration>
             .ToList();
         foreach (var derivedType in typeList)
         {
-            var getTypeName = derivedType.GetProperty("Type").GetValue(null) as string;
+            var getDrivedType = derivedType.GetProperty("Type");
+
+            if (getDrivedType == null)
+                throw new InvalidOperationException($"Something went wrong getting property info of \"Type\" of type, {type.FullName}");
+
+            var getTypeName = getDrivedType.GetValue(null) as string;
             _types.Add(getTypeName, derivedType);
         }
     }
@@ -63,24 +68,29 @@ public class TypeDiscriminatorConverter : JsonConverter<IStorageConfiguration>
         */
     if (reader.TokenType != JsonTokenType.StartObject)
         {
-            throw new JsonException();
+            throw new JsonException($"Tried to read config, expected start of object but got {reader.TokenType} at {reader.TokenStartIndex}, {reader.Position}, {reader.TokenType}");
         }
 
         using (var jsonDocument = JsonDocument.ParseValue(ref reader))
         {
             if (!jsonDocument.RootElement.TryGetProperty("Type", out var typeProperty))
             {
-                throw new JsonException();
+                throw new JsonException($"Missing type element on node at {reader.Position}, {reader.TokenType} / {reader.TokenStartIndex} / {reader.TokenType}");
             }
 
-            Type type = null;
-            if (_types.TryGetValue(typeProperty.GetString(), out type) == false)
+            Type type;
+            var tryGetString = typeProperty.GetString();
+
+            if (tryGetString == null)
+                throw new JsonException($"Couldn't read type property  at {reader.Position}, {reader.TokenType} / {reader.TokenStartIndex} / {reader.TokenType}");
+
+            if (_types.TryGetValue(tryGetString, out type) == false)
             {
-                throw new JsonException();
+                throw new JsonException($"Couldn't read type property  at {reader.Position}, {reader.TokenType} / {reader.TokenStartIndex} / {reader.TokenType}");
             }
 
             var jsonObject = jsonDocument.RootElement.GetRawText();
-            var result = (IStorageConfiguration)JsonSerializer.Deserialize(jsonObject, type, options);
+            var result = (IStorageConfiguration)JsonSerializer.Deserialize(jsonObject, type, options)!;
 
             return result;
         }
